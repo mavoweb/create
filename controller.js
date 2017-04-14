652b1ed187993b2a538722a8c67eafc3f508b78e
Francesca Cicileo <fcicileo@mit.edu>
2017-04-14 14:18:59 -0400

    File uploading using DOM elements instead of string parsing
    Requires modified version of mavo.js to account for issue with location of
    about:srcdoc.

--- controller.js
+++ controller.js
@@ -5,49 +5,10 @@ var iframe = $("#mavo_display");
 var uploadButton = $("#upload_input_html_file");
 var inputFile = $("#input_html_file");
 
-iframe.onload = function() {
-	//iframe.contentDocument.write(textarea.value);
-
-    var potentialHeadText = textarea.value.split("<head>");
-    if (potentialHeadText.length > 1) {
-    	var headText = potentialHeadText[1].split("</head>");
-    	if (headText.length > 1) {
-    		var finalHeadText = headText[0];
-    		if (!finalHeadText.includes("mavo.js") || !finalHeadText.includes("mavo.css")) {
-    			iframe.contentDocument.head.innerHTML += finalHeadText;
-			} else {
-    			iframe.contentDocument.head.innerHTML = finalHeadText;
-    		}
-    	} else {
-    		//Bad HTML?
-    	}
-    } else { //no head element; use default with just Mavo
-    	//iframe.contentDocument.head.innerHTML = "<script src='//mavo.io/mavo/mavo.js'></script><link rel='stylesheet' href='//mavo.io/mavo/mavo.css'/>";
-    }
-
-    var potentialBodyText = textarea.value.split("<body>");
-    if (potentialBodyText.length > 1) {
-    	var bodyText = potentialBodyText[1].split("</body>");
-    	if (bodyText.length > 1) {
-    		iframe.contentDocument.body.innerHTML = bodyText[0];
-    	} else {
-    		//Bad HTML?
-    	}
-    } //else no body element
-
-    //TODO: find a better way to read HTML from iFrame to put in textarea
-    // var textareaNewContent = "<head>\n" + iframe.contentDocument.head.innerHTML + "\n</head>\n<body>\n" + iframe.contentDocument.body.innerHTML + "\n</body>\n";
-    // textarea.value = textareaNewContent
-
-    var Mavo = iframe.contentWindow.Mavo;
-
-    if (Mavo && !Mavo.all.length) {
-        Mavo.init();
-    }
-};
 
 textarea.oninput = function() {
     iframe.contentWindow.location.href = iframe.src; 
+    iframe.srcdoc = textarea.value;
 };
 
 //consulted http://stackoverflow.com/questions/34212240/reading-uploaded-text-file-contents-in-variable
@@ -58,8 +19,9 @@ uploadButton.onclick = function() {
         var reader = new FileReader();
 
         reader.onload = function(e) {
+            //set up text in textarea
             textarea.value = reader.result;
-            iframe.contentWindow.location.href = iframe.src; 
+            iframe.srcdoc = textarea.value;
         };
         reader.readAsText(file);    
     }
