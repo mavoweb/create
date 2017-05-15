6ad4c3ede4c1bbb5226f5d715c95df6259fff0ec
Francesca Cicileo <fcicileo@mit.edu>
2017-05-14 22:26:34 -0400

    Initial buttons for adding a property name. Add mv-app and mv-storage to html. Adds a preset property name to an element.

--- controller.js
+++ controller.js
@@ -12,34 +12,56 @@ var changeToMavoDisplay = $("#change_to_mavo_display");
 var changeToBothDisplay = $("#change_to_both_display");
 
 iframe.onload = function() {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(textarea.value, "text/html");
+    //add Mavo to page if it's not there already
     if (!iframe.contentWindow.Mavo) {
-        $.create("script", {src: "//mavo.io/mavo/mavo.js", inside: iframe.contentDocument.head });
-        $.create("link", {rel:"stylesheet", href:"//mavo.io/mavo/mavo.css", inside: iframe.contentDocument.head });
-        // $.include(iframe.contentWindow.Mavo, "//mavo.io/mavo/mavo.js").then(function(){
-        //         if (Mavo.inited) {
-        //            Mavo.init();
-        //         }
-        // });
+        $.create("script", {src: "//get.mavo.io/mavo.js", inside: iframe.contentDocument.head });
+        $.create("link", {rel:"stylesheet", href:"//get.mavo.io/mavo.css", inside: iframe.contentDocument.head });
+        iframe.contentDocument.body.innerHTML = "";
+        var mvAppDivWrapper = $.create("div", {'mv-app':'', 'mv-storage':"local", innerHTML:doc.body.innerHTML, inside: iframe.contentDocument.body });
         textarea.value = iframe.contentDocument.documentElement.outerHTML;
     }
 
-    iframe.contentWindow.document.body.onmousemove = function(e) {
-        e.target.style.outline = "5px solid black";
+    var allElements = iframe.contentDocument.querySelectorAll('body *');
+    allElements.forEach(function(e) {
+        e.onmouseover = function(evt) {
+            evt.target.style.outline = "5px solid black";
+        };
+        e.onmouseout = function(evt) {
+            evt.target.style.outline = "";
+        };
+        e.onclick = function(evt) {
+            evt.stopPropagation();
+            evt.target.style.outline = "5px solid red";
 
-        //listener to remove outline when mouse leaves element
-        e.target.onmouseleave = function(evt) {
-            evt.currentTarget.style.outline = "";
-        }
+            var elemToRemove = iframe.contentDocument.getElementById("creator-toolbar");
+            if (elemToRemove) {
+                elemToRemove.remove();
+            }
 
-        //remove outline from parent elements
-        var a =  e.target.parentNode;
-        var els = [];
-        while (a && a.tagName !== "HTML") {
-            console.log(a);
-            a.style.outline = "";
-            a = a.parentNode;
+            
+            var givePropButton = $.create("button", {id:"give-property", textContent:"Give a Property Name"});
+            var makeMultipleButton = $.create("button", {id:"make-multiple", textContent:"Allow for Multiple Copies"});
+            var insertEquationButton = $.create("button", {id:"insert-equation", textContent:"Insert an Equation"});
+            
+            $.create("div", {id:"creator-toolbar", contents:[givePropButton, makeMultipleButton, insertEquationButton], inside: iframe.contentDocument.body});
+        
+            givePropButton.onclick = function() {
+                // var newPropNameInput = $.create("input", {type:"text"});
+                // $.create("div", {id:"new_prop_name_setter", contents:["Choose a property name:", newPropNameInput], inside: iframe.contentDocument.body});
+                evt.target.setAttribute("property", "newPropName");
+                textarea.value = iframe.contentDocument.documentElement.outerHTML;
+                var elemToRemove = iframe.contentDocument.getElementById("creator-toolbar");
+                evt.target.style.outline = "";
+                if (elemToRemove) {
+                    elemToRemove.remove();
+                }
+            };
+
         }
-    }
+    });
+
 };
 
 textarea.oninput = function() {
